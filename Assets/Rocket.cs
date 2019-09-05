59a0104b8d3fc38ece3856a2b8fc48123cc8e925
Ihshan <ihgumilar@gmail.com>
2019-09-05 20:22:01 +1200

    Coordinate sys hand
    Applying left handed coordinate (cartesian). Vector3.forward
    (Using Time.delta_Chapter)

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start() {
-        
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         ProcessInput();
     }
 
@@ -19,16 +23,16 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
-            {
-                print("Rotating right");
-            }
+        {
+            transform.Rotate(-Vector3.forward);
+        }
     }
 }
